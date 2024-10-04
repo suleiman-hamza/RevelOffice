bcca5ad445cf64a026479c5caef8fe9488947425
suleiman hamza <hamzasuleiman530@gmail.com>
2024-10-04 01:29:25 +0100

    installed tailwind and shadcn vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Hello world</h1>
+
+  <Button>Click me</Button>
 </template>
 
 <style scoped>
@@ -27,4 +21,5 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
 </style>
