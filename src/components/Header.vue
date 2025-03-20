60143cefa5613e9594c087aede4ec3611100a0d9
suleiman hamza <hamzasuleiman530@gmail.com>
2025-03-20 12:08:27 +0100

    [hotfix]: menu toggle bug fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <nav class="nav-body">
         <Sheet>
                 <SheetTrigger as-child>
-                    <Button class="flex-initial btn" @click="toggleCart">
+                    <Button class="flex-initial btn">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#202020" width="18px" >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
@@ -58,7 +58,7 @@
                 </svg>
             </Button>
             <Transition name="scaleup">
-                <div class="hamburgerMenu" v-if="toggleState" ref="outsidemenu">
+                <div class="hamburgerMenu" v-if="toggleState">
                     <span><RouterLink to="/">Home</RouterLink></span>
                     <span class="comingsoon"><RouterLink to="/test">Track Orders</RouterLink></span>
                     <span><RouterLink to="/checkout">Checkout</RouterLink></span>
@@ -80,7 +80,6 @@ import {
 } from '@/components/ui/sheet';
 import { onClickOutside } from '@vueuse/core'
 import { Button } from '@/components/ui/button'
-import { ScrollArea } from '@/components/ui/scroll-area'
 import { ref, watch } from 'vue'
 
 
@@ -95,10 +94,10 @@ const outsidemenu = ref(null);
 // const openMenu = ref(false);
 const toggleState = ref(false)
 
-watch(route, () => {
-    toggleState.value = false
-    console.log('kinda')
-})
+// watch(route, () => {
+//     toggleState.value = false
+//     console.log('kinda')
+// })
 
 // onClickOutside(target, () => {
 //     openMenu.value = false;
@@ -106,6 +105,7 @@ watch(route, () => {
 
 onClickOutside(outsidemenu, () => {
     toggleState.value = false
+    console.log(toggleState.value)
 })
 
 // function toggleCart() {
@@ -114,13 +114,14 @@ onClickOutside(outsidemenu, () => {
 // }
 
 function toggleMenu() {
-    toggleState.value = !toggleState.value
+    toggleState.value = !toggleState.value;
     console.log(toggleState.value)
 }
 
 function checkoutRoute() {
     router.push('/checkout')
 }
+
 </script>
 
 <style lang="css" scoped>
